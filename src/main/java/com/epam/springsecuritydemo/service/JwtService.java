bc51f937869f907dfd5b062eb9cf2f08b8d1ef3d
shasank gadipilli <gadipillishasank@gmail.com>
2023-08-08 17:31:43 +0530

    basic Spring security authentication

---
+++ src/main/java/com/epam/springsecuritydemo/service/JwtService.java
@@ -0,0 +1,19 @@
+package com.epam.springsecuritydemo.service;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtService {
+
+    public String generateToken(String username) {
+        Map<String,Object> claims = new HashMap<>();
+        return createToken(claims,username);
+    }
+
+    private String createToken(Map<String, Object> claims, String username) {
+        return Jw
+    }
+}
